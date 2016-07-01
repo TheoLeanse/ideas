d2ebe42d57a654f1b964088b6e69eec772eea6d5
TheoLeanse <theoleanse@gmail.com>
2016-07-01 23:07:32 +0100

    adding ideas

--- src/index.js
+++ src/index.js
@@ -3,26 +3,44 @@ import { createStore } from 'redux';
 import { Provider, connect } from 'react-redux';
 import { render } from 'react-dom';
 
-const initialState = {
-    ideas: [
-        {
-            title: 'a',
-            author: 'b',
-            description: 'c',
-            supporters: 'd',
-            buttonValue: 'e'
-        },
-        {
-            title: 'a',
-            author: 'b',
-            description: 'c',
-            supporters: 'd',
-            buttonValue: 'e'
-        }
-    ]
+const initialState = [
+    {
+        title: 'a',
+        author: 'b',
+        description: 'c',
+        supporters: 'd',
+        buttonValue: 'e'
+    },
+    {
+        title: 'a',
+        author: 'b',
+        description: 'c',
+        supporters: 'd',
+        buttonValue: 'e'
+    }
+];
+
+const acceptForm = ({ title, author, description }) => {
+    return {
+        title: title.value,
+        author: author.value,
+        description: description.value,
+        supporters: 'd',
+        buttonValue: 'e'
+    };
 };
 
-const reducer = (state, action) => initialState;
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_IDEA':
+            return [
+                ...state,
+                acceptForm(action.data)
+            ];
+        default:
+            return initialState;
+    }
+};
 
 const Idea = ({ idea }) => {
     return (
@@ -36,12 +54,45 @@ const Idea = ({ idea }) => {
     );
 };
 
-let App = ({ ideas }) => {
-    return <div>{ideas.map((idea, key) => <Idea key={ key } idea={ idea } /> ) }</div>;
+let IdeaSubmit = ({ handleSubmit }) => {
+    return (
+        <form onSubmit={ (e) => {
+            e.preventDefault();
+            handleSubmit(e.target);
+        }} >
+            <input type="text" name="title" />
+            <input type="text" name="author" />
+            <input type="text" name="description" />
+            <input type="submit" />
+        </form>
+    );
 };
 
-const mapStateToProps = ({ ideas }) => {
-    return { ideas };
+const mapStateToIdeaSubmitProps = () => {
+    return {};
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        handleSubmit(formData) {
+            console.log(dispatch, formData);
+            dispatch({type: 'ADD_IDEA', data: formData})
+        }
+    }
+}
+
+IdeaSubmit = connect(mapStateToIdeaSubmitProps, mapDispatchToProps)(IdeaSubmit);
+
+let App = ({ ideas }) => {
+    console.log(ideas);
+    return <div>
+        {ideas.map((idea, key) => <Idea key={ key } idea={ idea } /> ) }
+        <IdeaSubmit />
+    </div>;
+};
+
+const mapStateToProps = (state) => {
+    return { ideas: state };
 };
 
 App = connect(mapStateToProps)(App);
