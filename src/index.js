5a12ba333cb6b32c8ace73f9358c07743fdd3d14
TheoLeanse <theoleanse@gmail.com>
2016-07-08 23:28:56 +0100

    export single reducer

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { createStore, combineReducers } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { render } from 'react-dom';
-import { userReducer, ideasReducer, uiReducer } from './reducers';
+import reducer from './reducers';
 import { Router, hashHistory } from 'react-router';
 import routes from './routes';
 import './main.scss';
-
-const reducer = combineReducers({
-    user: userReducer,
-    ideas: ideasReducer,
-    nav: uiReducer
-});
 
 render(
     <Provider store={ createStore(reducer) }>
---
+++ src/reducers/initialIdeasState.js
@@ -0,0 +1,98 @@
+export default [
+    {
+        title: 'Developer corporate responsibility',
+        author: 'James',
+        description: 'Get employers to volunteer their developers\' time as part of their corporate responsibility programme',
+        supporters: ['Sue', 'Gareth', 'Clive', 'June'],
+        comments: [{}, {}],
+        id: 2
+    },
+    {
+        title: 'Test republic',
+        author: 'Charlie',
+        description: 'Design and build a community to test hypotheses around the structure of communities. Fund through exports. Experimentation. Desalination? Solar. Geodesic domes.',
+        supporters: ['Sue', 'Gareth', 'Clive', 'June'],
+        comments: [{}, {}, {}],
+        id: 1
+    },
+    {
+        title: 'Developer corporate responsibility',
+        author: 'James',
+        description: 'Get employers to volunteer their developers\' time as part of their corporate responsibility programme',
+        supporters: ['Sue', 'Gareth', 'Clive', 'June'],
+        comments: [{}, {}],
+        id: 2
+    },
+    {
+        title: 'Test republic',
+        author: 'Charlie',
+        description: 'Design and build a community to test hypotheses around the structure of communities. Fund through exports. Experimentation. Desalination? Solar. Geodesic domes.',
+        supporters: ['Sue', 'Gareth', 'Clive', 'June'],
+        comments: [{}, {}, {}],
+        id: 1
+    },
+    {
+        title: 'Developer corporate responsibility',
+        author: 'James',
+        description: 'Get employers to volunteer their developers\' time as part of their corporate responsibility programme',
+        supporters: ['Sue', 'Gareth', 'Clive', 'June'],
+        comments: [{}, {}],
+        id: 2
+    },
+    {
+        title: 'Test republic',
+        author: 'Charlie',
+        description: 'Design and build a community to test hypotheses around the structure of communities. Fund through exports. Experimentation. Desalination? Solar. Geodesic domes.',
+        supporters: ['Sue', 'Gareth', 'Clive', 'June'],
+        comments: [{}, {}, {}],
+        id: 1
+    },
+    {
+        title: 'Developer corporate responsibility',
+        author: 'James',
+        description: 'Get employers to volunteer their developers\' time as part of their corporate responsibility programme',
+        supporters: ['Sue', 'Gareth', 'Clive', 'June'],
+        comments: [{}, {}],
+        id: 2
+    },
+    {
+        title: 'Test republic',
+        author: 'Charlie',
+        description: 'Design and build a community to test hypotheses around the structure of communities. Fund through exports. Experimentation. Desalination? Solar. Geodesic domes.',
+        supporters: ['Sue', 'Gareth', 'Clive', 'June'],
+        comments: [{}, {}, {}],
+        id: 1
+    },
+    {
+        title: 'Developer corporate responsibility',
+        author: 'James',
+        description: 'Get employers to volunteer their developers\' time as part of their corporate responsibility programme',
+        supporters: ['Sue', 'Gareth', 'Clive', 'June'],
+        comments: [{}, {}],
+        id: 2
+    },
+    {
+        title: 'Test republic',
+        author: 'Charlie',
+        description: 'Design and build a community to test hypotheses around the structure of communities. Fund through exports. Experimentation. Desalination? Solar. Geodesic domes.',
+        supporters: ['Sue', 'Gareth', 'Clive', 'June'],
+        comments: [{}, {}, {}],
+        id: 1
+    },
+    {
+        title: 'Developer corporate responsibility',
+        author: 'James',
+        description: 'Get employers to volunteer their developers\' time as part of their corporate responsibility programme',
+        supporters: ['Sue', 'Gareth', 'Clive', 'June'],
+        comments: [{}, {}],
+        id: 2
+    },
+    {
+        title: 'Test republic',
+        author: 'Charlie',
+        description: 'Design and build a community to test hypotheses around the structure of communities. Fund through exports. Experimentation. Desalination? Solar. Geodesic domes.',
+        supporters: ['Sue', 'Gareth', 'Clive', 'June'],
+        comments: [{}, {}, {}],
+        id: 1
+    }
+];
