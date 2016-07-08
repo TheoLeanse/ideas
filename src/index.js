0cc64300ee99d3969fe82225c13c4d732cfacad3
TheoLeanse <theoleanse@gmail.com>
2016-07-09 00:05:39 +0100

    routes directory

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { render } from 'react-dom';
-import reducer from './reducers';
+import reducers from './reducers';
 import { Router, hashHistory } from 'react-router';
-import routes from './routes';
+import routes from './routes/';
 import './main.scss';
 
 render(
-    <Provider store={ createStore(reducer) }>
+    <Provider store={ createStore(reducers) }>
         <Router routes={ routes } history={ hashHistory } />
     </Provider>,
     document.getElementById('app')
---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route } from 'react-router';
+import {
+    IdeaFeed,
+    IdeaSubmit,
+    About,
+    Register,
+    Login,
+    App
+} from '../components';
+
+export default (
+        <Route component={ App }>
+        <Route path='/' component={ IdeaFeed }/>
+        <Route path='/new' component={ IdeaSubmit }/>
+        <Route path='/about' component={ About }/>
+        <Route path='/register' component={ Register }/>
+        <Route path='/login' component={ Login }/>
+        </Route>
+);
